7b4556a2745e7261c533a5efff3c91516e71e4fa
JagvirH <jheer02@gmail.com>
2024-05-06 21:25:58 +0100

    Added components to create page

--- app/(root)/Create/page.jsx
+++ app/(root)/Create/page.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from 'react';
 
 export default async function Page() {
-
     return (
         <div className='p-16'>
             <div className='bg-[#FFFFFF] rounded p-2'>
@@ -26,6 +25,19 @@ export default async function Page() {
                             placeholder='Enter description...'
                         ></textarea>
                     </div>
+                    <div className='mb-4'>
+                        <label className='block text-sm font-medium text-gray-700'>Tags</label>
+                        <select
+                            className='mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+                            multiple={true}
+                            size={4} // Display 4 options at a time
+                        >
+                            <option value='eyes'>Eyes</option>
+                            <option value='head'>Head</option>
+                            <option value='stomach'>Stomach</option>
+                            <option value='cold'>Cold</option>
+                        </select>
+                    </div>
                     <button
                         type='submit'
                         className='w-full bg-[#1479fd] py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
@@ -33,13 +45,7 @@ export default async function Page() {
                         Submit
                     </button>
                 </form>
-
-
-
             </div>
         </div>
-        
-    )
+    );
 }
-
-
