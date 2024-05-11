e519af5a4fb30154002b4eb1b3d05b8958ec4d59
JagvirH <jheer02@gmail.com>
2024-05-11 19:14:04 +0100

    Altered SQL database and created query to update log

--- app/(root)/Create/page.jsx
+++ app/(root)/Create/page.jsx
@@ -1,7 +1,5 @@
-'use client'
-
 import { getTag } from '@/backend/database/tags';
-import React, { useState } from 'react';
+
 import {logForm} from '@/backend/validation/form'
 import CreateLogForm from '@/components/forms/createLogForm';
 
--- backend/database/logs.js
+++ backend/database/logs.js
@@ -3,3 +3,19 @@
 import mysql from 'mysql2';
 import { connectToDB } from "@/backend/database/mySql";
 
+export async function addLog({userId, title, description}) {
+    let connection = await connectToDB();
+
+    const sql = 'INSERT INTO logs (Users_id, Title, Description) VALUES (?,?,?)'
+    
+    try {
+        await connection.query(sql, [userId, title, description])
+        console.log("ADDED")
+    } catch (error) {
+        console.log("Error with adding log: ", error)
+    } finally {
+        connection.close();
+    }
+
+}
+
