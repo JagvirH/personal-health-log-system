5a155e5394a6dfa89680391aa3243eb4a68fa7a7
JagvirH <jheer02@gmail.com>
2024-05-14 19:44:56 +0100

    Small changes for the search page

--- components/forms/searchLogForm.jsx
+++ components/forms/searchLogForm.jsx
@@ -26,20 +26,36 @@ export default function SearchLogForm() {
     }
     return (
         <div className='bg-[#FFFFFF] rounded p-2'>
-            <form className='p-2' onSubmit={handleSubmit}>
-                <div className='mb-4'>
-                    <label className='block text-sm font-medium text-gray-700'>Type in what your initial assessment is</label>
-                        <textarea
-                            value={description}
-                            key='description'
-                            onChange={(e) => setDescription(e.target.value)}
-                            className='mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-40 resize-none'
-                            placeholder='Enter description...'
-                        ></textarea>
-                </div>
-            
+            <div className='flex flex-row'>
+                <div className='w-3/4'>                
+                    <form className='p-2' onSubmit={handleSubmit}>
+                        <div className='mb-4'>
+                            <label className='block text-sm font-medium text-gray-700'>Type in what your initial assessment is</label>
+                                <textarea
+                                    value={description}
+                                    key='description'
+                                    onChange={(e) => setDescription(e.target.value)}
+                                    className='mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm h-40 resize-none'
+                                    placeholder='Enter description...'
+                                ></textarea>
+                        </div> 
 
-            </form>
+                    </form>
+                </div>
+                <div className='flex w-1/4'>
+                    <div className='flex flex-col p-4'>
+                        <div className='h-1/2 justify-center w-full blue_button p-2'>
+                            Create log
+                        </div>
+                        <div className='h-1/2 justify-center blue_button pt-2'>
+                            Search
+                        </div>
+
+                    </div>
+                
+                </div>
+            </div>
+            
 
         </div>
     );
