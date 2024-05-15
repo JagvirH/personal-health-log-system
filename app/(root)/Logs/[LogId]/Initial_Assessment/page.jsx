2a4d6deda8f6aa5b46dcf220422eaed224df45b0
JagvirH <jheer02@gmail.com>
2024-05-15 21:13:35 +0100

    comments out auth checks to avoid errors for now

--- app/(root)/Logs/[LogId]/Initial_Assessment/page.jsx
+++ app/(root)/Logs/[LogId]/Initial_Assessment/page.jsx
@@ -1,18 +1,14 @@
+"use client"
+
 import { checkIfUsersLog } from '@/backend/database/logs';
 import React from 'react'
+import { useRouter } from "next/navigation";
 import { auth, currentUser } from "@clerk/nextjs/server";
 
 const Page = async ({ params }) => {
 
-    const logId = params.LogId
-    const user = await currentUser()
-    
-    if(!user) return null;
-    const userId = user?.id;
+    //console.log(checkIfLogEqUsers)
 
-    const checkIfLogEqUsers = await checkIfUsersLog({userId,logId})
-
-    console.log(checkIfLogEqUsers)
 
     //console.log(params.LogId)
     return(
--- app/(root)/Logs/[LogId]/layout.jsx
+++ app/(root)/Logs/[LogId]/layout.jsx
@@ -1,11 +1,34 @@
+"use client"
+
 import { Inter } from "next/font/google";
 import "../../../globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
+import { useRouter } from "next/navigation";
+import { auth, currentUser } from "@clerk/nextjs/server";
 
-export default function RootLayout({ children }) {
+
+
+
+export default async function RootLayout({ children, params }) {
+  /*
+  const router = useRouter();
+
+  const logId = params.LogId
+  const user = await currentUser()
+  
+  if(!user) return null;
+  const userId = user?.id;
+
+  const checkIfLogEqUsers = await checkIfUsersLog({userId,logId})
+
+  //console.log(checkIfLogEqUsers)
+
+  */
+
   return (
     <div>
-      {children}
+      {children }
     </div>
   );
+
 }
