0800fb9dc819e3c68337feafab68c7741028ba98
JagvirH <jheer02@gmail.com>
2024-05-25 13:00:34 +0100

    small changes

--- app/(root)/Logs/[LogId]/Initial_Assessment/page.jsx
+++ app/(root)/Logs/[LogId]/Initial_Assessment/page.jsx
@@ -1,10 +1,21 @@
-"use client"
-
 import React from 'react'
+import { ClerkProvider } from '@clerk/nextjs'
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { checkIfUsersLog } from '@/backend/database/logs';
 
 const Page = async ({ params }) => {
 
+    //const router = useRouter();
 
+    const logId = params.LogId
+    const user = await currentUser()
+    
+    if(!user) return null;
+    const userId = user?.id;
+
+    const checkIfLogEqUsers = await checkIfUsersLog({userId,logId})
+
+    //console.log(checkIfLogEqUsers)
 
     return(
         <div>
