ee2937354bd4ef7cce581b142f2179e4137c1075
JagvirH <jheer02@gmail.com>
2024-06-05 19:52:32 +0100

    The status can now be changed, need to fx it on the backend side

--- components/forms/LogEdit.jsx
+++ components/forms/LogEdit.jsx
@@ -6,31 +6,34 @@ import { useRouter } from "next/navigation";
 const LogEdit = ({ Id, description, status }) => {
   const router = useRouter();
   const [currentDescription, setCurrentDescription] = useState(description);
+  const [currentStatus, setCurrentStatus] = useState(status);
   const [isChanged, setIsChanged] = useState(false);
 
   useEffect(() => {
-    setIsChanged(currentDescription !== description);
-  }, [currentDescription, description]);
+    setIsChanged(currentDescription !== description || currentStatus !== status);
+  }, [currentDescription, currentStatus, description, status]);
 
-  const handleChange = (e) => {
+  const handleDescriptionChange = (e) => {
     setCurrentDescription(e.target.value);
   };
 
-  const handleSubmit = async(e) => {
-    e.preventDefault();
-    console.log('New Description:', currentDescription);
-
-    await editLogDescription({
-        Id : Id,
-        Description: currentDescription
-    })
-
-    router.push("/Logs");
-
+  const handleStatusChange = (e) => {
+    setCurrentStatus(e.target.value);
   };
 
-  console.log(status)
-  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('New Description:', currentDescription);
+    console.log('New Status:', currentStatus);
+
+    await editLogDescription({
+      Id: Id,
+      Description: currentDescription,
+      Status: currentStatus
+    });
+
+    router.push("/Logs");
+  };
 
   return (
     <div className='flex flex-col'>
@@ -41,8 +44,19 @@ const LogEdit = ({ Id, description, status }) => {
         <textarea
           className='bg-white rounded-xl p-2 w-full h-48'
           value={currentDescription}
-          onChange={handleChange}
+          onChange={handleDescriptionChange}
         />
+        <div className='mt-4'>
+          Status:
+          <select
+            className='bg-white rounded-xl p-2 w-full'
+            value={currentStatus}
+            onChange={handleStatusChange}
+          >
+            <option value='Ongoing'>Ongoing</option>
+            <option value='Solved'>Solved</option>
+          </select>
+        </div>
         <button
           type='submit'
           className={`mt-2 p-2 rounded ${isChanged ? 'bg-blue-500 text-white cursor-pointer' : 'bg-gray-300 text-gray-700 cursor-not-allowed'}`}
@@ -51,9 +65,6 @@ const LogEdit = ({ Id, description, status }) => {
           Save
         </button>
       </form>
-      <div className='bg-[green]'>
-        {status}
-      </div>
     </div>
   );
 };
