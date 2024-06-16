e89315aafb2d9ffe2f4bfce5256b5011787c9436
JagvirH <jheer02@gmail.com>
2024-06-16 15:46:06 +0100

    design changes to add delete button and bookmark to log pages

--- app/(root)/Logs/[LogId]/layout.jsx
+++ app/(root)/Logs/[LogId]/layout.jsx
@@ -5,6 +5,7 @@ import { currentUser } from "@clerk/nextjs/server";
 import { getlog } from "@/backend/database/logs";
 import Image from 'next/image';
 import LogSidebar from "@/components/layout_design/LogSidebar";
+//import Image from 'next/image';
 
 
 
@@ -21,8 +22,8 @@ export default async function RootLayout({ children, params }) {
               <div className="bg-white p-4 text-[50px] rounded-xl border border-grey ">
                 {log.Title} 
               </div>
-              <div className="pt-4">
-                <div className="border-grey p-2 flex items-center">
+              <div className="pt-4 flex flex-row">
+                <div className="border-grey p-2 flex items-center w-4/5">
                         <div className="justify-center items-center p-1 md-text">Tags: </div>
                         {log.Tags.map(tag => (
                             <span key={tag.Id} className="tag-item px-1 ">
@@ -31,6 +32,20 @@ export default async function RootLayout({ children, params }) {
                                 </div>  
                             </span>
                         ))}
+                  </div>
+                  <div className="w-[200px] h-full px-2">
+                    <div className="blue_button">
+                      Delete
+                    </div>
+                  </div>
+                  <div className="">
+                    <Image 
+                      src={"/bookmark-solid.svg"}
+                      alt="BookMark" 
+                      width={40} 
+                      height={40} 
+                      className="hover-image"
+                    />
                   </div>
               </div>
 
