289e913c0237eb689060b913c43191e890df47c1
JagvirH <jheer02@gmail.com>
2024-06-30 21:14:17 +0100

    made the list a button

--- components/Cards/LogBookmarkCard.jsx
+++ components/Cards/LogBookmarkCard.jsx
@@ -1,36 +1,38 @@
 import React from 'react';
-import Image from 'next/image';
+import { useRouter } from "next/navigation";
 
 const LogBookmarkCard = ({ title, status, description, id }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log(id);
+    router.push(`/Logs/${id}/Initial_Assessment`)
+    window.location.reload();
+  };
+
   return (
-    <div className='border border-grey flex flex-row bg-white mb-2 p-4 rounded'>
+    <button
+      onClick={handleClick}
+      className='border border-gray-300 flex flex-row bg-white mb-2 p-4 rounded w-full text-left hover:border-blue-500'
+    >
       <div className='w-3/4'>
         <div className='flex flex-col'>
           <div className='font-bold'>
             {title}
           </div>
-          <div className='border border-gray-300 mt-2 p-2 text-sm max-h-12 overflow-hidden rounded-xl'>
-            <p className='line-clamp-3 pb-1'>{description}</p>
+          <div className='border border-gray-300 mt-2 p-2 text-sm max-h-12 overflow-hidden'>
+            <p className='line-clamp-3'>{description}</p>
           </div>
         </div>
       </div>
-      <div className='w-1/4 flex items-center justify-center p-4'>
-        <div className='flex flex-row border border-blue-500 p-2 rounded'>
-          <div className='text-sm'>
+      <div className='w-1/4 flex flex-col items-center justify-center p-4'>
+        <div className='border border-blue-500 p-2 rounded'>
+          <div>
             Straight to <br /> journey
           </div>
-          <div className='items-center justify-center'>
-            <Image
-              src={"/arrow-right-solid.svg"}
-              alt="Arrow"
-              width={20}
-              height={20}
-              className="hover-image"
-            />
-          </div>
         </div>
       </div>
-    </div>
+    </button>
   );
 };
 
--- components/layout_design/HomeLogs.jsx
+++ components/layout_design/HomeLogs.jsx
@@ -1,9 +1,12 @@
+"use client";
 import React from 'react';
 import LogBookmarkCard from '../Cards/LogBookmarkCard';
+import { useRouter } from "next/navigation";
 
 const HomeLogs = ({ logs }) => {
+  
   return (
-    <div className='space-y-2'>
+    <div className='space-y-2 overflow-y-auto h-full'>
       {logs && logs.length > 0 ? (
         logs.map((log, index) => (
           <LogBookmarkCard key={index} title={log.Title} status={log.Status} description={log.Description} id={log.Id} />
