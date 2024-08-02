0d570667c2dda38f2e59cf5a03508994c4b2ad6a
JagvirH <jheer02@gmail.com>
2024-08-02 23:39:28 +0100

    .

--- backend/database/statTesting.js
+++ backend/database/statTesting.js
@@ -3,3 +3,9 @@
 import mysql from 'mysql2';
 import { connectToDB } from "@/backend/database/mySql";
 import { getLogsId } from './logs';
+
+export async function getLogsIssues(){
+    let connection = await connectToDB();
+
+    const sql = 'SELECT * FROM'
+}
