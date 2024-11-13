b1f01471971d9713ae227166a155c5455db8e1d8
JagvirH <jheer02@gmail.com>
2024-11-13 17:21:13 +0000

    clerk fix

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware({
-    
+  publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
 });
 
 export const config = {
